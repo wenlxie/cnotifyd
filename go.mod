57e59da6b72e9c2fbdd9e8a2971bec0702f4225a
s3rj1k <evasive.gyron@gmail.com>
2019-11-20 14:13:18 +0200

    PoW
    Signed-off-by: s3rj1k <evasive.gyron@gmail.com>

---
+++ go.mod
@@ -0,0 +1,8 @@
+module /github.com/s3rj1k/cnotifyd
+
+go 1.13
+
+require (
+	github.com/s3rj1k/go-fanotify/fanotify v0.0.0-20191125112157-eb8b6ee54e02
+	golang.org/x/sys v0.0.0-20191120155948-bd437916bb0e
+)
---
+++ signal.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+
+	"github.com/s3rj1k/go-fanotify/fanotify"
+	"golang.org/x/sys/unix"
+)
+
+// Exit is a helper to do cleanup on exit.
+func Exit(notify *fanotify.NotifyFD, sig os.Signal) {
+	Info.Printf("cnotifyd: caught signal '%s': shutting down\n", sig)
+
+	_ = os.Remove(unixSocketPath)
+
+	if notify != nil {
+		if err := notify.Mark(
+			unix.FAN_MARK_FLUSH|
+				unix.FAN_MARK_MOUNT,
+			0, -1, "",
+		); err != nil {
+			Error.Printf("cnotifyd: failed to flush marks: %v\n", err)
+		}
+	}
+}
